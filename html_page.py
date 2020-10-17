41578d44d74fddeed48a4e293ca6e0cbc2c227f7
Mike Zozu <jaat-quod@yandex.ru>
2020-10-17 06:34:56 +0300

    Better Russian?

--- html_page.py
+++ html_page.py
@@ -29,10 +29,10 @@ index_page = f'''
         <legend>Для слов из файла (должен быть в UTF-8):</legend>
         <input type=file id="{WORDS_FILE_INPUT}" name="{WORDS_FILE_INPUT}">
         <br>
-        <label for="{SHUFFLE_CHECKBOX}">Перемешать слова:</label>
+        <label for="{SHUFFLE_CHECKBOX}">Перемешать:</label>
         <input type=checkbox id="{SHUFFLE_CHECKBOX}" name="{SHUFFLE_CHECKBOX}">
         <br>
-        <label for="{COUNT_INPUT}">Макс. полей (0=неограниченно):</label>
+        <label for="{COUNT_INPUT}">Макс. число полей (0=неогр.):</label>
         <input type=number min=0 max=200 value=0 id="{COUNT_INPUT}" name="{COUNT_INPUT}">
         <br>
         <input type=submit value="Сгенерировать!">
@@ -46,10 +46,10 @@ index_page = f'''
         <legend>Для слов из набора:</legend>
         <select id="{WORDLIST_INPUT}" name="{WORDLIST_INPUT}">{wordlist_selector_options(WORDLISTS)}</select>
         <br>
-        <label for="{SHUFFLE_CHECKBOX}">Перемешать слова:</label>
+        <label for="{SHUFFLE_CHECKBOX}">Перемешать:</label>
         <input type=checkbox id="{SHUFFLE_CHECKBOX}" name="{SHUFFLE_CHECKBOX}" checked=true>
         <br>
-        <label for="{COUNT_INPUT}">Макс. полей (0=неограниченно):</label>
+        <label for="{COUNT_INPUT}">Макс. число полей (0=неогр.):</label>
         <input type=number min=0 max=200 value=0 id="{COUNT_INPUT}" name="{COUNT_INPUT}">
         <br>
         <input type=submit value="Сгенерировать!">
@@ -63,10 +63,10 @@ index_page = f'''
         <legend>Для слов из текстовой формы:</legend>
         <textarea name={WORDS_TEXT_INPUT} rows=8 style="width:400px"></textarea>
         <br>
-        <label for="{SHUFFLE_CHECKBOX}">Перемешать слова:</label>
+        <label for="{SHUFFLE_CHECKBOX}">Перемешать:</label>
         <input type=checkbox id="{SHUFFLE_CHECKBOX}" name="{SHUFFLE_CHECKBOX}">
         <br>
-        <label for="{COUNT_INPUT}">Макс. полей (0=неограниченно):</label>
+        <label for="{COUNT_INPUT}">Макс. число полей (0=неогр.):</label>
         <input type=number min=0 max=200 value=0 id="{COUNT_INPUT}" name="{COUNT_INPUT}">
         <br>
         <input type=submit value="Сгенерировать!">
